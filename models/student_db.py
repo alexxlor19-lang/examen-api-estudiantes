7e328c742c58b4a0998ea5aef06e698ad01249c2
alexxlor19-lang <alexxlor19@gmail.com>
2026-05-18 23:18:00 -0500

    student_2

--- models/student_db.py
+++ models/student_db.py
@@ -8,9 +8,9 @@ class StudentDB(Base):
 
     ID = Column(Integer, primary_key=True, index=True, autoincrement=True)
     DNI = Column(String, unique=True, nullable=False, index=True) # Obligatorio y único
-    NAME = Column(String, nullable=False) [cite: 30]
-    AGE = Column(Integer, nullable=False) [cite: 31]
-    GRADE = Column(Float, nullable=False) [cite: 32]
+    NAME = Column(String, nullable=False)
+    AGE = Column(Integer, nullable=False)
+    GRADE = Column(Float, nullable=False)
     is_approved = Column(Boolean, default=False, nullable=False) # No automático
     
     #marcas de tiempo
